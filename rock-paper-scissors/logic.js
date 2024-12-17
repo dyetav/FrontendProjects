811085f2e18e4392f3c0b325e3a0e192ed9a1f61
Diego <dtavolaro@vulog.com>
2024-12-17 23:40:39 +0100

    rock paper scissors over
    rock paper scissors over

--- rock-paper-scissors/logic.js
+++ rock-paper-scissors/logic.js
@@ -1,5 +1,64 @@
-let wins = 0;
-let lost = 0;
-let draw = 0;
+const you_choice = document.querySelector(".choices .you-choice .images");
+const computer_choice = document.querySelector(".choices .computer-choice .images");
+const win = document.querySelector(".results .result .win");
+const lost = document.querySelector(".results .result .lost");
+const draw = document.querySelector(".results .result .draw");
+const resetButton = document.querySelector(".buttons .reset");
 
-document.querySelector(".results").innerHTML = `Wins ${wins} | Lost ${lost} | Draw ${draw}`;
+const images = document.querySelectorAll(".images-container .images img");
+
+const inputMap = {
+    "scissors": "scissors-emoji.png",
+    "paper": "paper-emoji.png",
+    "rock": "rock-emoji.png"
+}
+
+const inputArray = ["scissors", "paper", "rock"];
+
+function resetScore() {
+    win.textContent = "0";
+    lost.textContent = "0";
+    draw.textContent = "0";   
+    you_choice.textContent = '';
+    computer_choice.textContent = '';
+}
+
+function getComputerChoice() {
+    const randomValue = Math.floor(Math.random() * inputArray.length);
+    return inputArray[randomValue];
+}
+
+function updateResults(you, computer) {
+    let computerWin = false;
+    let youWin = false;
+    if (computer == you) {
+        draw.textContent = Number(draw.textContent) + 1;
+    } else if ((you == "scissors" && computer == "rock") || (you == "rock" && computer == "paper") || (you == "paper" && computer == "scissors")) {
+        lost.textContent = Number(lost.textContent) + 1;
+    } else {
+        win.textContent = Number(win.textContent) + 1;
+    }
+    
+}
+
+for (let image of images) {
+    image.addEventListener("click", (e) => {
+        you_choice.textContent = '';
+        computer_choice.textContent = '';
+        const inputYou = e.target.id;
+        const nodeYouChoiceImg = document.createElement("img");
+        nodeYouChoiceImg.src = "../images/" + inputYou + "-emoji.png";
+        you_choice.append(nodeYouChoiceImg); 
+
+        const nodeComputerChoiceImg = document.createElement("img");
+        const inputComputer = getComputerChoice();
+        nodeComputerChoiceImg.src = "../images/" + inputComputer + "-emoji.png";
+        computer_choice.append(nodeComputerChoiceImg);
+
+        updateResults(inputYou, inputComputer);
+    });
+}
+
+resetButton.addEventListener("click", () => {
+    resetScore();
+});
